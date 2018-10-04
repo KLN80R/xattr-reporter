88bfbc78d6ae58fa03f40c3c707dec90fd35908c
Kaylen Payer <kln80r@gmail.com>
2018-10-04 12:59:03 +1000

    Clean up formatting

--- proj.py
+++ proj.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python2.7
 
 import xattr
+import re
 from pathlib import Path
 
 PATH = Path('/Users/kp/Downloads')  # Replace as appropriate
 
 files = [p for p in PATH.iterdir() if p.is_file()]
 for p in files:
+
     print "\\" + "="*50 + "\\"
-    print "\n* Listing extended attributes for: " + str(p) + "\n"
-    x = xattr.listxattr(str(p))
-    print str(x)
-    print "\n*"
+    print "\n* Listing extended attributes for: \n\t" + str(p) + "\n"
+
+    x = str(xattr.listxattr(str(p)))
+    x = re.sub('[() \']', '', x)
+
+    list = x.split(",")
+    for l in list:
+        print l
+
+    print "\n"
+
+print "\\" + "="*50 + "\\"
