8d866707d19810fb423f3de039403a755219e1b0
sarahagb <sarahagbulos@gmail.com>
2018-10-07 05:11:41 +0000

    download path is now required as an argument

--- proj.py
+++ proj.py
@@ -1,24 +1,29 @@
-#!/usr/bin/python2.7
-
-import xattr
+import sys
 import re
+import xattr
 from pathlib import Path
 
-PATH = Path('/Users/kp/Downloads')  # Replace as appropriate
 
-files = [p for p in PATH.iterdir() if p.is_file()]
-for p in files:
+if __name__ == "__main__":
 
-    print "\\" + "="*50 + "\\"
-    print "\n* Listing extended attributes for: \n\t" + str(p) + "\n"
+	if len(sys.argv) != 2:
+		print "Usage: python proj.py <downloads folder>"
+		exit(1)
 
-    x = str(xattr.listxattr(str(p)))
-    x = re.sub('[() \']', '', x)
+	PATH = Path(sys.argv[1])
+	
+	files = [p for p in PATH.iterdir() if p.is_file()]
+	for p in files:
+		print "\\" + "="*50 + "\\"
+		print "\n* Listing extended attributes for: \n\t" + str(p) + "\n"
+	
+		x = str(xattr.listxattr(str(p)))
+		x = re.sub('[() \']', '', x)
+		
+		list = x.split(",")
+		for l in list:
+			print l
+		
+		print "\n"
 
-    list = x.split(",")
-    for l in list:
-        print l
-
-    print "\n"
-
-print "\\" + "="*50 + "\\"
+	print "\\" + "="*50 + "\\"
