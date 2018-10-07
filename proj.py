375a09ee986cde16f9b2622660846f80c1c8a3b9
sarahagb <sarahagbulos@gmail.com>
2018-10-07 10:08:32 +0000

    prints out a list of xattr details for each file in directory

--- proj.py
+++ proj.py
@@ -1,8 +1,9 @@
+import os
 import sys
-import re
+import time
 import xattr
 from pathlib import Path
-
+	
 
 if __name__ == "__main__":
 
@@ -10,20 +11,28 @@ if __name__ == "__main__":
 		print "Usage: python proj.py <downloads folder>"
 		exit(1)
 
-	PATH = Path(sys.argv[1])
-	
-	files = [p for p in PATH.iterdir() if p.is_file()]
-	for p in files:
-		print "\\" + "="*50 + "\\"
-		print "\n* Listing extended attributes for: \n\t" + str(p) + "\n"
-	
-		x = str(xattr.listxattr(str(p)))
-		x = re.sub('[() \']', '', x)
-		
-		list = x.split(",")
-		for l in list:
-			print l
-		
-		print "\n"
+	path = Path(sys.argv[1])
 
-	print "\\" + "="*50 + "\\"
+	#TODO add flag for whether to sort descending or ascending
+
+	# get extended attributes for each file in given directory
+	files = [p for p in path.iterdir() if p.is_file()]
+	xattrs = []
+	for p in files:
+		file_xattrs = {}
+		
+		file_xattrs['file_path'] = str(p)
+		file_xattrs['create_time'] = time.ctime(os.path.getctime(str(p)))
+		file_xattrs['extended_attributes'] = []
+	
+		x = xattr.listxattr(str(p))
+		for attr_name in x:
+			file_xattrs['extended_attributes'].append({str(attr_name):xattr.getxattr(str(p), str(attr_name))})
+		
+		xattrs.append(file_xattrs)
+
+	# sort list by time descending
+	xattrs.sort(key=lambda x:x['create_time'], reverse=True)
+
+	print xattrs
+
