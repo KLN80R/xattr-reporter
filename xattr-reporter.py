ea783b089acf6cd3457e68434cd5333e9af09655
sarahagb <sarahagbulos@gmail.com>
2018-10-14 08:56:57 +0000

    added optional flag for sorting by time ascending

--- xattr-reporter.py
+++ xattr-reporter.py
@@ -11,8 +11,6 @@ from markdown import markdown
 # Generate a markdown report of ADS data per file
 def genReport(xattrs, path, file_name):
 
-	#TODO Make this write out to a file
-
 	r = open(file_name + '.md', 'w')
 
 	r.write("## Extended Attributes Report for " + str(path) + "\n\n")
@@ -43,7 +41,7 @@ def toPDF(file_name):
 
 
 # get extended attributes for each file in a given directory and return dict
-def get_xattrs(path):
+def get_xattrs(path, asc=False):
 	
 	files = [p for p in path.iterdir() if p.is_file()]
 	xattrs = []
@@ -60,23 +58,27 @@ def get_xattrs(path):
 
 		xattrs.append(file_xattrs)
 
-	# sort list by time descending
-	xattrs.sort(key=lambda x:x['create_time'], reverse=True)
+	if asc:
+		# sort by create time ascending
+		xattrs.sort(key=lambda x:x['create_time'])
+	else:
+		# sort by create time descending
+		xattrs.sort(key=lambda x:x['create_time'], reverse=True)
 
 	return xattrs
 
 
-# Main
 if __name__ == "__main__":
 
 	parser = argparse.ArgumentParser()
 	parser.add_argument("path", help="path to directory to be scanned for extended attributes", type=str)
 	parser.add_argument("-r", "--recursive", action="store_true", help="recursively scan for extended attributes")
-	parser.add_argument("-o", "--output", type=str, help="name of output file")
+	parser.add_argument("-o", "--output", type=str, help="name of file to output to")
+	parser.add_argument("-a", "--ascending", action="store_true", help="sort by time ascending (default is descending)")
 	args = parser.parse_args()
 
 	path = Path(args.path)
-	xattrs = get_xattrs(path)
+	xattrs = get_xattrs(path, args.ascending)
 	
 	if args.output:
 		genReport(xattrs, path, args.output)
