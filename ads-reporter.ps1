231a83a776cf7db4e1032057456a406ab7d77ccc
min-maxwell <z3464892@ad.unsw.edu.au>
2018-10-17 23:01:35 +1100

    fixed up json output

--- ads-reporter.ps1
+++ ads-reporter.ps1
@@ -1,10 +1,10 @@
 ﻿param (
     [string]$path = ".\",
     [string]$output,
-    [switch]$recurse = $false
+    [switch]$recurse = $false,
+    [switch]$dl
 )
 
-$adsMap = @{}
 
 # set file name if output filename given
 if ($output) { $json_file = '.\'+$output+'.json' }
@@ -13,43 +13,86 @@ if ($output) { $json_file = '.\'+$output+'.json' }
 if ($recurse) { $files = Get-ChildItem -Path $path -Attributes !Directory -Force -Recurse }
 else { $files = Get-ChildItem -Path $path -Attributes !Directory -Force }
 
-foreach ($f in $files)
-{
-    # initalise hashmap
-    $adsMap[$f]=@()
 
-    # get all data streams -- somehow this actually gets all ads per file
-    $alternateData = Get-Item -Path $f.FullName | Get-Item -Stream *
+### this function takes a list of files and returns a hash of all ads items from that list
+function getAllADS ($files)
+{
+    $adsMap = @{}
+
+    foreach ($f in $files) {
+        # initalise hashmap
+        $adsMap[$f]=@()
     
-    foreach ($item in $alternateData) {
-        if ($item.Stream.Contains('DATA') -eq $true){continue}
-        $adsMap[$f] += $item
+        # get all data streams -- somehow this actually gets all ads per file
+        $alternateData = Get-Item -Path $f.FullName | Get-Item -Stream *
+        
+        foreach ($item in $alternateData) {
+            if ($item.Stream.Contains('DATA') -eq $true){continue}
+            $adsMap[$f] += $item
+        }
     }
+    return $adsMap
 }
 
-$jsonOutput = @{}
-foreach($key in $adsMap.Keys)
+### this function returns a hashmap of all ads items where ZoneId=3
+Function getDLHistory($adsMap)
 {
-    foreach($item in $adsMap[$key]) {
-        if (!$output) {
-            Write-Host "Path: " $item.PSPath -ForegroundColor Green
-            Write-Host "Creation Date: " $key.CreationTimeUtc  -ForegroundColor Green
-            Write-Host "PSChildName: " $item.PSChildName -ForegroundColor Red
-            Write-Host "Parent Path: " $item.PSParentPath -ForegroundColor Yellow
-            Write-Host "Filename: " $item.FileName -ForegroundColor Yellow
-            Write-Host "Length: " $item.Length -ForegroundColor Yellow
-            Write-Host "Content:" -ForegroundColor Cyan
-            Get-Content -Path $key.FullName -Stream $item.Stream
-            Write-Host "`n"
+    $dlMap = @{}
+    foreach($key in $adsMap.Keys) {
+        foreach($item in $adsMap[$key].Values) {
+            $content = Get-Content -Path $key.FullName -Stream $item.Stream
+            
+            if($content.Contains("ZoneId=3")) {
+                $dlMap[$key.FullName + $item.Stream.ToString()] = $item
+            }
         }
-        else {
-            $jsonOutput[$key.FullName + $item.Stream.ToString()] += @{ "File Name" = $key.Name
-            "Creation Date UTC" = $key.CreationTimeUTC.DateTime
-                                            "ADS Stream" = $item.Stream
-                                           }
+    }
+    return $dlMap
+}
+
+# dumps ads information to screen
+function writeADSToScreen($item, $key)
+{
+    Write-Host "Path: " $item.PSPath -ForegroundColor Green
+    Write-Host "Creation Date: " $key.CreationTimeUtc  -ForegroundColor Green
+    Write-Host "PSChildName: " $item.PSChildName -ForegroundColor Red
+    Write-Host "Parent Path: " $item.PSParentPath -ForegroundColor Yellow
+    Write-Host "Filename: " $item.FileName -ForegroundColor Yellow
+    Write-Host "Length: " $item.Length -ForegroundColor Yellow
+    Write-Host "Content:" -ForegroundColor Cyan
+    Get-Content -Path $key.FullName -Stream $item.Stream
+    Write-Host "`n"
+}
+
+# takes hash of ads and returns json report style hash
+function getJsonOutput($adsMap)
+{
+    $jsonOutput = @{}
+    foreach($key in $adsMap.Keys)
+    {
+        foreach($item in $adsMap[$key]) {
+            $jsonOutput[$key.FullName + $item.Stream.ToString()] += @{  "File Name" = $key.Name
+                                                                        "Creation Date UTC" = $key.CreationTimeUTC.DateTime
+                                                                        "ADS Stream" = $item.Stream
+                                                                     }
         }
+    }
+    return $jsonOutput
+}
+
+
+
+$adsMap = getALLADS($files)
+
+if ($output) {
+    if ($dl) { 
+        $dlmap = getDLHistory($adsMap);
+        $jsonOutput = getJsonOutput($dlmap)
+        Write-Output $jsonOutput | ConvertTo-Json | Out-File $json_file -Encoding utf8
+    } else {
+        $jsonOutput = getJsonOutput($adsMap)
+        Write-Output $jsonOutput | ConvertTo-Json | Out-File $json_file -Encoding utf8
     }
 }
 
-#foreach( $item in $jsonOutput.GetEnumerator() | Sort -Property "Creation Date UTC"
-if ($output) { Write-Output $jsonOutput | ConvertTo-Json | Out-File $json_file}
+#elseif ($output) { Write-Output $jsonOutput | ConvertTo-Json | Out-File $json_file -Encoding utf8}
