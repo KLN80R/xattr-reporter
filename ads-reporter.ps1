ac2c1300f0418e5d1f95f6ee1a8f1632b0055100
min-maxwell <z3464892@ad.unsw.edu.au>
2018-10-17 21:13:04 +1100

    spits out to json

--- ads-reporter.ps1
+++ ads-reporter.ps1
@@ -1,11 +1,18 @@
 ﻿param (
     [string]$path = ".\",
+    [string]$output,
     [switch]$recurse = $false
 )
-#$getPath = Read-Host "What is the path you would like to search?`n"
+
 $adsMap = @{}
+
+# set file name if output filename given
+if ($output) { $json_file = '.\'+$output+'.json' }
+
+# get list of files (uses recursion if option set)
 if ($recurse) { $files = Get-ChildItem -Path $path -Attributes !Directory -Force -Recurse }
 else { $files = Get-ChildItem -Path $path -Attributes !Directory -Force }
+
 foreach ($f in $files)
 {
     # initalise hashmap
@@ -14,29 +21,35 @@ foreach ($f in $files)
     # get all data streams -- somehow this actually gets all ads per file
     $alternateData = Get-Item -Path $f.FullName | Get-Item -Stream *
     
-    foreach ($item in $alternateData)
-    {
+    foreach ($item in $alternateData) {
         if ($item.Stream.Contains('DATA') -eq $true){continue}
         $adsMap[$f] += $item
-
     }
 }
 
+$jsonOutput = @{}
 foreach($key in $adsMap.Keys)
 {
-    foreach($item in $adsMap[$key])
-    {
-        Write-Host "Path: " $item.PSPath -ForegroundColor Green
-        Write-Host "Creation Date: " $key.CreationTimeUtc  -ForegroundColor Green
-        Write-Host "Parent Path: " $item.PSParentPath -ForegroundColor Yellow
-        Write-Host "PSChildName: " $item.PSChildName -ForegroundColor Yellow 
-        Write-Host "PSProvider: " $item.PSProvider -ForegroundColor Yellow
-        Write-Host "PSIsContainer: " $item.PSIsContainer -ForegroundColor Yellow
-        Write-Host "Filename: " $item.FileName -ForegroundColor Yellow
-        Write-Host "Stream: " $item.Stream -ForegroundColor Red
-        Write-Host "Length: " $item.Length -ForegroundColor Yellow
-        Write-Host "Content:" -ForegroundColor Cyan
-        Get-Content -Path $key.FullName -Stream $item.Stream
-        Write-Host "`n"
+    foreach($item in $adsMap[$key]) {
+        if (!$output) {
+            Write-Host "Path: " $item.PSPath -ForegroundColor Green
+            Write-Host "Creation Date: " $key.CreationTimeUtc  -ForegroundColor Green
+            Write-Host "PSChildName: " $item.PSChildName -ForegroundColor Red
+            Write-Host "Parent Path: " $item.PSParentPath -ForegroundColor Yellow
+            Write-Host "Filename: " $item.FileName -ForegroundColor Yellow
+            Write-Host "Length: " $item.Length -ForegroundColor Yellow
+            Write-Host "Content:" -ForegroundColor Cyan
+            Get-Content -Path $key.FullName -Stream $item.Stream
+            Write-Host "`n"
+        }
+        else {
+            $jsonOutput[$key.FullName + $item.Stream.ToString()] += @{ "File Name" = $key.Name
+            "Creation Date UTC" = $key.CreationTimeUTC.DateTime
+                                            "ADS Stream" = $item.Stream
+                                           }
+        }
     }
 }
+
+#foreach( $item in $jsonOutput.GetEnumerator() | Sort -Property "Creation Date UTC"
+if ($output) { Write-Output $jsonOutput | ConvertTo-Json | File $json_file}
