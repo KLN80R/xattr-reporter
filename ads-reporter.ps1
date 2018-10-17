2123397c6b99cd3ca648888e98f625b0afac217f
min-maxwell <z3464892@ad.unsw.edu.au>
2018-10-17 20:02:18 +1100

    refactored

--- ads-reporter.ps1
+++ ads-reporter.ps1
@@ -1,15 +1,29 @@
 ﻿$getPath = Read-Host "What is the path you would like to search?`n"
+$adsMap = @{}
 
 $files = Get-ChildItem -Path $getPath -Attributes !Directory -Force -Recurse
 foreach ($f in $files)
 {
-    # use '$array += $element' to add item to end of array dynamically
+    # initalise hashmap
+    $adsMap[$f]=@()
+
+    # get all data streams -- somehow this actually gets all ads per file
     $alternateData = Get-Item -Path $f.FullName | Get-Item -Stream *
+    
     foreach ($item in $alternateData)
     {
         if ($item.Stream.Contains('DATA') -eq $true){continue}
+        $adsMap[$f] += $item
+
+    }
+}
+
+foreach($key in $adsMap.Keys)
+{
+    foreach($item in $adsMap[$key])
+    {
         Write-Host "Path: " $item.PSPath -ForegroundColor Green
-        Write-Host "Creation Date: " $f.CreationTimeUtc 
+        Write-Host "Creation Date: " $key.CreationTimeUtc  -ForegroundColor Green
         Write-Host "Parent Path: " $item.PSParentPath -ForegroundColor Yellow
         Write-Host "PSChildName: " $item.PSChildName -ForegroundColor Yellow 
         Write-Host "PSProvider: " $item.PSProvider -ForegroundColor Yellow
@@ -18,7 +32,7 @@ foreach ($f in $files)
         Write-Host "Stream: " $item.Stream -ForegroundColor Red
         Write-Host "Length: " $item.Length -ForegroundColor Yellow
         Write-Host "Content:" -ForegroundColor Cyan
-        Get-Content -Path $f.FullName -Stream $item.Stream
+        Get-Content -Path $key.FullName -Stream $item.Stream
         Write-Host "`n"
     }
 }
