7b6a4e5e25c7e8a97e7052c4e51a8fba43570b68
min-maxwell <z3464892@ad.unsw.edu.au>
2018-10-17 19:25:30 +1100

    powershell script to scrape and dump ads info

---
+++ ads-reporter.ps1
@@ -0,0 +1,23 @@
+﻿$getPath = Read-Host "What is the path you would like to search?`n"
+
+$files = Get-ChildItem -Path $getPath -Attributes !Directory -Force -Recurse
+foreach ($f in $files)
+{
+    # use '$array += $element' to add item to end of array dynamically
+    $alternateData = Get-Item -Path $f.FullName | Get-Item -Stream *
+    foreach ($item in $alternateData)
+    {
+        if ($item.Stream.Contains('DATA') -eq $true){continue}
+        Write-Host "Path: " $item.PSPath -ForegroundColor Green 
+        Write-Host "Parent Path: " $item.PSParentPath -ForegroundColor Yellow
+        Write-Host "PSChildName: " $item.PSChildName -ForegroundColor Yellow 
+        Write-Host "PSProvider: " $item.PSProvider -ForegroundColor Yellow
+        Write-Host "PSIsContainer: " $item.PSIsContainer -ForegroundColor Yellow
+        Write-Host "Filename: " $item.FileName -ForegroundColor Yellow
+        Write-Host "Stream: " $item.Stream -ForegroundColor Red
+        Write-Host "Length: " $item.Length -ForegroundColor Yellow
+        Write-Host "Content:" -ForegroundColor Cyan
+        Get-Content -Path $f.FullName -Stream $item.Stream
+        Write-Host "`n"
+    }
+}
